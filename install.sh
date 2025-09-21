4cb207c863f4e83a58d70339447567b0bb634076
Alistair <larrybarrygarry@gmail.com>
2025-09-21 19:15:08 +0800

    updating requirements + install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#cd to the parent directory of this script
+cd "$(dirname "$0")"/..
+pwd
+sudo apt-get install python3.10
 python3.10 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
